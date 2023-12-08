346a6425d1f6618e244e7296fa5cb729ad2fd3f3
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-12-07 22:46:21 -0500

    Handle values of HOOD_158

--- data-modelling.py
+++ data-modelling.py
@@ -9,17 +9,16 @@ Created on Thu Nov 30 11:52:43 2023
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import joblib
 
 from datetime import datetime
 from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
+from sklearn.utils import resample
 from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, confusion_matrix, ConfusionMatrixDisplay
-from sklearn.model_selection import cross_val_score, KFold, train_test_split, GridSearchCV
+from sklearn.model_selection import cross_val_score, KFold, train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.utils import resample
 
 data = pd.read_csv("./bicycle-thefts-open-data.csv")
 
@@ -40,6 +39,7 @@ print(len(data) - data.count())
 bike_make_list = data["BIKE_MAKE"].value_counts()
 bike_model_list = data["BIKE_MODEL"].value_counts()
 bike_colour_list = data["BIKE_COLOUR"].value_counts()
+hood_list = data["HOOD_158"].value_counts()
 categorial_imputer = SimpleImputer(
     missing_values=np.nan, strategy="most_frequent")
 median_imputer = SimpleImputer(missing_values=np.nan, strategy="median")
@@ -77,9 +77,9 @@ unknown_model_list = [
     "UNKOWN", "UNKONWN", "UNKNOWN - 1991",
     "UNKNOWN", "UNKNOW", "UNKNONW",
     "UNKNO", "UNKN PARTICULAR", "UNKN",
-    "UNKJNOWN", "UNK", "UN",
-    "UKN", "UK", "U/K",
-    "0", "----", "--", "-", "+"
+    "UNKJNOWN", "UNK", "UKN",
+    "UK", "U/K", "0",
+    "----", "--", "-", "+"
 ]
 
 data["BIKE_MODEL"].replace(unknown_model_list, "UNKNOWN", inplace=True)
@@ -97,14 +97,16 @@ data["BIKE_COLOUR"] = imputed_bike_colour_data
 data["BIKE_SPEED"].replace(0, np.nan, inplace=True)
 # Reshape 1D array to 2D array
 bike_speed_data = data["BIKE_SPEED"].values.reshape(-1, 1)
+# fill NaN value with median value
 imputed_bike_speed_data = median_imputer.fit_transform(bike_speed_data)
 data["BIKE_SPEED"] = imputed_bike_speed_data
 
 # BIKE_COST
 # replace zero with NaN
 data["BIKE_COST"].replace(0, np.nan, inplace=True)
-# fill NaN value with median value
+# Reshape 1D array to 2D array
 bike_cost_data = data["BIKE_COST"].values.reshape(-1, 1)
+# fill NaN value with median value
 imputed_bike_cost_data = median_imputer.fit_transform(bike_cost_data)
 data["BIKE_COST"] = imputed_bike_cost_data
 
@@ -115,8 +117,13 @@ data["OCC_MONTH"] = data["OCC_MONTH"].apply(
 data["REPORT_MONTH"] = data["REPORT_MONTH"].apply(
     lambda month_name: datetime.strptime(month_name, "%B").month)
 
+# HOOD_158
+data["HOOD_158"].replace("NSA", 0, inplace=True)
+data["HOOD_158"] = pd.to_numeric(data["HOOD_158"])
+
 # add RECOVERED column
-data["RECOVERED"] = [1 if status == "RECOVERED" else 0 for status in data["STATUS"]]
+data["RECOVERED"] = [1 if status ==
+                     "RECOVERED" else 0 for status in data["STATUS"]]
 data.drop("STATUS", axis=1, inplace=True)
 
 # balance the data
@@ -128,14 +135,15 @@ upsampled_data_minority = resample(
     data_minority,
     replace=True,
     n_samples=33923,
-    random_state=123
+    random_state=1
 )
 
 data = pd.concat([data_majority, upsampled_data_minority])
 print(data["RECOVERED"].value_counts())
 
 # transform categorical data to numeric
-categoricals = [col for col, col_type in data.dtypes.items() if col_type == "O"]
+categoricals = [col for col, col_type in data.dtypes.items()
+                if col_type == "O"]
 print(categoricals)
 for col in categoricals:
     data[col] = encoder.fit_transform(data[col])
@@ -146,7 +154,8 @@ column_names = features.columns
 scaled_features = scaler.fit_transform(features)
 features = pd.DataFrame(scaled_features, columns=column_names)
 
-x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(
+    features, target, test_size=0.2)
 
 # Logistic Regression
 # lr_model = LogisticRegression()
@@ -164,7 +173,9 @@ x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=
 # LR_search.best_params_
 # y_pred = LR_search.best_estimator_.predict(x_test)
 
-lr_model = LogisticRegression(solver="liblinear", C=0.001, penalty="l1", random_state=1)
+lr_model = LogisticRegression(
+    solver="liblinear", C=0.001, penalty="l1", random_state=1)
+
 lr_model.fit(x_train, y_train)
 
 crossvalidation = KFold(n_splits=10, shuffle=True, random_state=1)
@@ -173,16 +184,17 @@ score = np.mean(cross_val_score(lr_model, x_train, y_train,
                 scoring="accuracy", cv=crossvalidation, n_jobs=1))
 print('The score of the 10 fold run is: ', score)
 
-y_pred = lr_model.predict(x_test)
+lr_y_pred = lr_model.predict(x_test)
 
-print('Classification Report(N): \n', classification_report(y_test, y_pred))
-print('Confusion Matrix(N): \n', confusion_matrix(y_test, y_pred))
-print('Accuracy(N): \n', accuracy_score(y_test, y_pred))
-print("ROC-AUC:", roc_auc_score(y_test, y_pred))
+print('Classification Report(N): \n', classification_report(y_test, lr_y_pred))
+print('Confusion Matrix(N): \n', confusion_matrix(y_test, lr_y_pred))
+print('Accuracy(N): \n', accuracy_score(y_test, lr_y_pred))
+print("ROC-AUC:", roc_auc_score(y_test, lr_y_pred))
 
 plt.style.use("ggplot")
-cm = confusion_matrix(y_test, y_pred, labels=lr_model.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=lr_model.classes_)
+cm = confusion_matrix(y_test, lr_y_pred, labels=lr_model.classes_)
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm, display_labels=lr_model.classes_)
 disp.plot()
 plt.title("Logistic Regression Confusion Matrix")
 plt.xlabel("Predicted")
@@ -191,26 +203,31 @@ plt.show()
 
 joblib.dump(lr_model, "./lr_model.pkl")
 print("Model dumped!")
+joblib.dump(y_test, "./lr_y_test.pkl")
+print("Test target dumped!")
+joblib.dump(lr_y_pred, "./lr_y_pred.pkl")
+print("Prediction target dumped!")
 
 # Decision Trees
 dt_model = DecisionTreeClassifier(
-    criterion='entropy', max_depth=5, min_samples_split=20, random_state=99)
+    criterion='entropy', max_depth=10, min_samples_split=20, random_state=1)
 
 dt_model.fit(x_train, y_train)
 score = np.mean(cross_val_score(dt_model, x_train, y_train,
                 scoring="accuracy", cv=crossvalidation, n_jobs=1))
 print('The score of the 10 fold run is: ', score)
 
-y_pred = dt_model.predict(x_test)
+dt_y_pred = dt_model.predict(x_test)
 
-print('Classification Report(N): \n', classification_report(y_test, y_pred))
-print('Confusion Matrix(N): \n', confusion_matrix(y_test, y_pred))
-print('Accuracy(N): \n', accuracy_score(y_test, y_pred))
-print("ROC-AUC:", roc_auc_score(y_test, y_pred))
+print('Classification Report(N): \n', classification_report(y_test, dt_y_pred))
+print('Confusion Matrix(N): \n', confusion_matrix(y_test, dt_y_pred))
+print('Accuracy(N): \n', accuracy_score(y_test, dt_y_pred))
+print("ROC-AUC:", roc_auc_score(y_test, dt_y_pred))
 
 plt.style.use("ggplot")
-cm = confusion_matrix(y_test, y_pred, labels=dt_model.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=dt_model.classes_)
+cm = confusion_matrix(y_test, dt_y_pred, labels=dt_model.classes_)
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm, display_labels=dt_model.classes_)
 disp.plot()
 plt.title("Decision Trees Confusion Matrix")
 plt.xlabel("Predicted")
@@ -219,3 +236,7 @@ plt.show()
 
 joblib.dump(lr_model, "./dt_model.pkl")
 print("Model dumped!")
+joblib.dump(y_test, "./dt_y_test.pkl")
+print("Test target dumped!")
+joblib.dump(dt_y_pred, "./dt_y_pred.pkl")
+print("Prediction target dumped!")
